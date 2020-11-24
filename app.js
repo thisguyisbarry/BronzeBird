8504b783fb484e14a452ed25a43e5d3386f8a44b
thisguyisbarry <thisguyisbarry@gmail.com>
2020-11-24 20:24:14 +0000

    Fixed any conflicts (I hope)

--- app.js
+++ app.js
@@ -6,157 +6,17 @@ const session = require('express-session');
 
 const app = express();
 
-<<<<<<< HEAD
 const bodyParser = require('body-parser');
 const { body, validationResult } = require('express-validator');
 
 const insertCharacter = 'INSERT INTO Characters (characterName, characterRace, characterClass, characterLevel, characterAlignment) VALUES ($1, $2, $3, $4, $5);';
 const selectCharacters = "SELECT characterID, characterName, characterRace, characterClass, characterLevel, characterAlignment FROM Characters;";
-=======
-// passport
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-// bycrypt
-const bcrypt = require('bcrypt');
-
-// body parser
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// express validator
-const { body, validationResult } = require('express-validator');
-
-/**
- * Register
- */
-app.post('/register', [
-    // email validation
-    body('email')
-    .notEmpty()
-    .isEmail()
-    .normalizeEmail(),
-
-    // username validation
-    body('username')
-    .notEmpty()
-    .isAlphanumeric(),
-
-    // password validation
-    body('password')
-    .isLength({ min: 8 })
-
-], function(req, res) {
-    console.log("register");
-
-    const errors = validationResult(req);
-
-    if (errors.isEmpty()) { // if the email, username and password are valid
-        email = req.body.email;
-        username = req.body.username;
-        password = req.body.password;
-
-        // check if email is already registered
-        const emailQuery = db.prepare('SELECT Email FROM Users WHERE Email = $1;');
-        emailQuery.get(email, function(err, row) {
-            if (row) {
-                console.log("There is already a user registered with this email");
-                return;
-            }
-        })
-        
-        // check if username is taken
-        const userQuery = db.prepare('SELECT Username FROM Users WHERE Username = $1;');
-        userQuery.get(username, function(err, row) {
-            if (row) {
-                console.log("This username is not available");
-                return;
-            }
-        })
-
-        // insert email, username and encrypted password into database
-        bcrypt.hash(password, 10, function(err, hash) {
-            const insert = db.prepare('INSERT INTO Users (email, username, password) VALUES ($1, $2, $3);');
-            insert.run(email, username, hash);
-            insert.finalize();
-        });
-
-        console.log("Successfully registered");
-    }
-    else {
-        return res.status(400).json({ errors: errors.array() });
-    }
-})
-
-
-/**
- * Login
- */
-app.post('/login', function(req, res, next) {
-    console.log("login");
-    // passport local authentication
-    passport.authenticate('local', { 
-        successRedirect: '/',
-        failureRedirect: '/login',
-        failureFlash: true 
-    });
-});
-
-app.use(session({
-    secret: "CookieKeyGoesHere",
-    resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(
-    function (username, password, done) {
-        // select username aand password from database
-        const query = db.prepare('SELECT Username, Password FROM Users WHERE Username = $1;');
-        query.get(username, function (err, row) {
-            if (err) {
-                return done(err);
-            }
-            if (!row) {
-                return done(null, false, { message: 'User not found.' });
-            }
-            // compare username enmcrypted password
-            bcrypt.compare(password, row.password, function (err, result) {
-                if (result == true) {
-                    console.log("Login successful");
-                    done(null, { username: row.username });
-                } else {
-                    console.log("Login failed");
-                    return done(null, false, { message: 'Incorrect password' });
-                }
-            });
-        });
-    }
-));
-
-passport.serializeUser(function (user, done) {
-    return done(null, user.username);
-});
-
-passport.deserializeUser(function(username, done) {
-    const query = db.prepare('SELECT Username FROM Users WHERE Username = $1;');
-    query.get(username, function(err, row) {
-        done(err, row);
-    });
-});
-
-app.listen(3000, function () {
-    console.log("Server running on port 3000");
-});
->>>>>>> c280d7f1e869b00fb5c7c42a56e148c12a43ec98
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-<<<<<<< HEAD
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true }));
 
@@ -213,9 +73,6 @@ app.post("/submitCharacter", [
 
 
 
-=======
-app.use(express.static(__dirname + "/public"));
->>>>>>> c280d7f1e869b00fb5c7c42a56e148c12a43ec98
 
 
 /*  MUST BE AT END OF GETS
